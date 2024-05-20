a8f2c34c764e720bbb5889d4e259139b3090ad9f
tiagomatsushima <dev.matsu22@gmail.com>
2024-05-20 18:49:28 -0300

    Desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+public class program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Smartphone Iphone");
+        Smartphone iphone = new Iphone("123456789","147258369", "Iphone 12", 256 );
+        iphone.Ligar();
+        iphone.InstalarAplicativo("Instagram");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone Nokia");
+        Smartphone nokia = new Nokia("987654321", "963852741", "Nokia Eterno", 256);
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+    }
+}
+
+
+
